a9e401e848aa0a0fb19070221a19094af6cca3ae
bustomario <37638345+bustomario@users.noreply.github.com>
2020-03-23 15:24:22 -0400

    Modulo Usuarios

--- create_user.php
+++ create_user.php
@@ -1,7 +1,24 @@
 <?php
 
+    include 'conexionBD.php';
+    CrearUsuarios("Leticia","1980/12/18","letib@live.com","123456");
+    CrearUsuarios("Dora Isabel","1965/12/28","dorita@live.com","123456");
+    CrearUsuarios("Robert Blaires","1976/07/02","robertblaires@live.com","123456");
 
+    function CrearUsuarios($Nombre,$FechaNacimiento,$UserName,$Password){
+    IniciarConexion();
+    $Consulta="Select * from usuario where Username='".$UserName."'";
+    $Resultado= mysqli_num_rows($GLOBALS['Conexion']->query($Consulta));
+    if($Resultado==0){
+        $Consulta = "INSERT INTO usuario (Nombre, FechaNacimiento, Username, Password)
+        VALUES ('".$Nombre."', '".$FechaNacimiento."', '".$UserName."', '".password_hash($Password, PASSWORD_BCRYPT)."')";
 
-
-
+        if ($GLOBALS['Conexion']->query($Consulta) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $GLOBALS['Conexion']->error;
+        }
+    }
+    DesactivarConexion();
+    }
  ?>
